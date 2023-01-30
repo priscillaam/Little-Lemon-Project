52c243143c2cae71523fc7e4fd1fae0ba1925c02
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2023-01-30 12:33:13 -0500

    Grid layout of little lemon web app set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header.js';
+import Nav from './components/Nav.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer';
+import './styles/style.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="wrapper">
+      <div className="header"><Header /><Nav /></div>
+      <div className="main"><Main /></div>
+      <div className="footer"><Footer /></div>
     </div>
   );
 }
